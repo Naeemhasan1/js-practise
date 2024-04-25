3a7b97a230e2132085687e545c86531799b0d731
Naeemhasan1 <csnaeem4b@gmail.com>
2024-04-25 22:01:12 +0600

    Complete ejs first project

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -4,7 +4,16 @@ const app = expess();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { type: "it's a weekday", advice: " go to work ⚒️" });
+  const d = new Date("April 27, 2024 01:15:00");
+  const today = d.getDay();
+  let type = "it's a weekday";
+  let adv = " go to work ⚒️";
+  console.log(today);
+  if (today === 0 || today === 6) {
+    type = "it's weekend";
+    adv = " watch netflix 🍿🎞️";
+  }
+  res.render("index.ejs", { dayType: type, advice: adv });
 });
 
 app.listen(port, () => {
